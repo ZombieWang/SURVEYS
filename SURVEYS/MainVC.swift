ad3673dfa9ef82657c0a29295353b0447d0d9502
Frank <frank7300228@gmail.com>
2017-09-18 17:17:01 +0800

    Finish navigation and swipe functionality

--- SURVEYS/MainVC.swift
+++ SURVEYS/MainVC.swift
@@ -13,27 +13,44 @@ class MainVC: UIViewController {
     @IBOutlet weak var pagerCollectionView: UICollectionView!
     
     var items = [String]()
+    var currentIndicatorIndex = 0 {
+        didSet {
+            collectionView(cardCollectionView, didSelectItemAt: IndexPath(row: currentIndicatorIndex, section: 0))
+        }
+    }
+    
+    var currentItemIndex: Int = 0
+    var downwardCellSum = 0
+    var upwardCellSum = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationController?.navigationBar.tintColor = .white
         
         if #available(iOS 10.0, *) {
             cardCollectionView.isPrefetchingEnabled = false
         }
         
-        for i in 1 ... 5 {
+        navigationController?.navigationBar.tintColor = .white
+        
+        for i in 1 ... 30 {
             items.append("\(i)")
         }
         
         cardCollectionView.dataSource = self
         cardCollectionView.delegate = self
-        cardCollectionView.decelerationRate = UIScrollViewDecelerationRateFast
+        if let layout = cardCollectionView.collectionViewLayout as? CardLayout {
+            layout.collectionViewCellDelegate = self
+        }
         
         pagerCollectionView.dataSource = self
         pagerCollectionView.delegate = self
+        
+        cardCollectionView.decelerationRate = UIScrollViewDecelerationRateFast
         pagerCollectionView.decelerationRate = UIScrollViewDecelerationRateFast
+    }
+    
+    func didSelectIndicator() {
+        collectionView(cardCollectionView, didSelectItemAt: IndexPath(row: currentIndicatorIndex, section: 0))
     }
     
     @IBAction func refreshTapped(_ sender: UIBarButtonItem) {
@@ -54,6 +71,11 @@ extension MainVC: UICollectionViewDataSource {
             
             return cell
         } else if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DequeuePagerCell, for: indexPath) as? PagerCell {
+            if currentIndicatorIndex == indexPath.row {
+                cell.imageView.image = #imageLiteral(resourceName: "selected")
+            } else {
+                cell.imageView.image = #imageLiteral(resourceName: "notSelect")
+            }
             
             return cell
         }
@@ -63,5 +85,48 @@ extension MainVC: UICollectionViewDataSource {
 }
 
 extension MainVC: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // MARK: I use collectionView and cell with fixed width and height for pager. The collectionView is 30*420 and cell is 30*30, which means the collectionView can have 14 cells within it at most. When the user's scrolling the cardCollectionView and the indicator cell hits the upper or lower boundary, content offset needs to be changed. After reseting the offset, the sums needs to be reset to max value(maxCellsQty) as well because it is still on the boundary. In addition, the dots also provide navigation functionality. Press the dot  the content view will navigate to the index accordingly.
+        if collectionView === pagerCollectionView {
+            if indexPath.row > currentIndicatorIndex {
+                downwardCellSum += indexPath.row - currentIndicatorIndex
+                upwardCellSum = upwardCellSum > 0 ? upwardCellSum - (indexPath.row - currentIndicatorIndex) : 0
+            } else if indexPath.row < currentIndicatorIndex {
+                upwardCellSum += currentIndicatorIndex - indexPath.row
+                downwardCellSum = downwardCellSum > 0 ? downwardCellSum - (currentIndicatorIndex - indexPath.row) : 0
+            }
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DequeuePagerCell, for: indexPath)
+            let maxCellsQty = Int(collectionView.bounds.height / cell.bounds.height - 1)
+            
+            if downwardCellSum >= maxCellsQty && indexPath.row > currentIndicatorIndex {
+                collectionView.setContentOffset(CGPoint(x: 0, y: collectionView.contentOffset.y + 30 * CGFloat(downwardCellSum - maxCellsQty)), animated: true)
+                downwardCellSum = maxCellsQty
+            } else if upwardCellSum >= maxCellsQty && indexPath.row < currentIndicatorIndex {
+                collectionView.setContentOffset(CGPoint(x: 0, y: collectionView.contentOffset.y - 30 * CGFloat(upwardCellSum - maxCellsQty)), animated: true)
+                upwardCellSum = maxCellsQty
+            }
+            
+            performSelector(onMainThread: #selector(didSelectIndicator), with: nil, waitUntilDone: false)
+            
+            DispatchQueue.main.async {
+                self.pagerCollectionView.reloadData()
+            }
+            
+            currentIndicatorIndex = indexPath.row
+        } else if collectionView === cardCollectionView, let layout = collectionView.collectionViewLayout as? CardLayout {
+            let offset = layout.dragOffset * CGFloat(indexPath.item)
+            if collectionView.contentOffset.y != offset {
+                collectionView.setContentOffset(CGPoint(x: 0, y: offset), animated: true)
+            }
+            
+            currentItemIndex = indexPath.row
+        }
+    }
+}
+
+extension MainVC: CollectionViewCellDelegate {
+    func didCellIndexChange(index: CGFloat) {
+        collectionView(pagerCollectionView, didSelectItemAt: IndexPath(row: Int(index), section: 0))
+    }
 }
