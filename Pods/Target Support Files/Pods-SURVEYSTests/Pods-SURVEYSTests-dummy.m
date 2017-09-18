c2d55cfdc2ba574c7cc2e7eaf4ee78aa9f5932a9
Frank <frank7300228@gmail.com>
2017-09-19 02:59:18 +0800

    Clear pods

--- Pods/Target Support Files/Pods-SURVEYSTests/Pods-SURVEYSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SURVEYSTests : NSObject
-@end
-@implementation PodsDummy_Pods_SURVEYSTests
-@end
--- Pods/Target Support Files/Pods-SURVEYSUITests/Pods-SURVEYSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SURVEYSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SURVEYSUITests
-@end
