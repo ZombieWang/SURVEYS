616901e273f4ec238addcbd267d4d82b3518b857
Frank <frank7300228@gmail.com>
2017-09-18 11:14:57 +0800

    Add two collectionviews for content display and paging indicator

---
+++ SURVEYS/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  SURVEYS
+//
+//  Created by Frank on 18/09/2017.
+//  Copyright © 2017 Frank. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
