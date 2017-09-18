4a836472de8bfac7237ec6907b675f24b105bd0e
Frank <frank7300228@gmail.com>
2017-09-18 15:13:02 +0800

    Add layout settings and finish cardCollectionView full-screen display

--- SURVEYS/CardCell.swift
+++ SURVEYS/CardCell.swift
@@ -14,4 +14,23 @@ class CardCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageCoverView: UIView!
     @IBOutlet weak var takeSurveyBtn: RoundedButton!
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        
+        let standardHeight = DeviceVariable.standardHeight
+        let featuredHeight = DeviceVariable.windowHeight
+        
+        let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
+        
+        let minAlpha: CGFloat = 0.3
+        let maxAlpha: CGFloat = 0.75
+        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+        
+        let scale = max(delta, 0.5)
+        
+        nameLbl.transform = CGAffineTransform(scaleX: scale, y: scale)
+        descriptionLbl.alpha = delta
+        takeSurveyBtn.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
 }
