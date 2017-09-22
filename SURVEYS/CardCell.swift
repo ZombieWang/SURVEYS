56f84f1eed01d5f6739ca4921e5c9b2e99d310bb
Frank <frank7300228@gmail.com>
2017-09-22 21:20:01 +0800

    Run swiftlint autocorrect in terminal for spaces indentation

--- SURVEYS/CardCell.swift
+++ SURVEYS/CardCell.swift
@@ -9,28 +9,28 @@
 import UIKit
 
 class CardCell: UICollectionViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet private weak var imageCoverView: UIView!
-    @IBOutlet weak var takeSurveyButton: RoundedButton!
-    
-    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
-        super.apply(layoutAttributes)
-        
-	    let standardHeight = DeviceVariable.standardHeight
-        let featuredHeight = DeviceVariable.windowHeight
-        
-	    let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
-        
-	    let minAlpha: CGFloat = 0.3
-	    let maxAlpha: CGFloat = 0.75
-        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
-        
-        let scale = max(delta, 0.5)
-        
-        titleLabel.transform = CGAffineTransform(scaleX: scale, y: scale)
-        descriptionLabel.alpha = delta
-        takeSurveyButton.transform = CGAffineTransform(scaleX: scale, y: scale)
-    }
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet private weak var imageCoverView: UIView!
+	@IBOutlet weak var takeSurveyButton: RoundedButton!
+	
+	override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+		super.apply(layoutAttributes)
+		
+		let standardHeight = DeviceVariable.standardHeight
+		let featuredHeight = DeviceVariable.windowHeight
+		
+		let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
+		
+		let minAlpha: CGFloat = 0.3
+		let maxAlpha: CGFloat = 0.75
+		imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+		
+		let scale = max(delta, 0.5)
+		
+		titleLabel.transform = CGAffineTransform(scaleX: scale, y: scale)
+		descriptionLabel.alpha = delta
+		takeSurveyButton.transform = CGAffineTransform(scaleX: scale, y: scale)
+	}
 }
